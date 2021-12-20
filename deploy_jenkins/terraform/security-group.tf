66b15d139a39f52d27f9e3de05d386d05431b68e
gitarena <allan.fernandes@gmail.com>
2021-12-19 23:12:33 -0300

    Initial Commit

---
+++ deploy_jenkins/terraform/security-group.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "sg_jenkins" {
+  name        = "sg_jenkins"
+  description = "Allow all outbound traffic and inbound 22/80"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "sg_jenkins"
+  }
+}
